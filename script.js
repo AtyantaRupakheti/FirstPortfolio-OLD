b3fadae081992e75653e39733ca08f67886862b8
Atyanta Rupakheti <atyantarupa@gmail.com>
2024-05-14 20:37:59 -0400

    Added a working contact me section

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function scrollToElement(elementSelector, instance = 0){
+function scrollToElement(elementSelector, instance = 0) {
     // Select all elements that match the given selector
     const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the sleector and if the requested instance exists
-    if(elements.length > instance){
+    // Check if there are elements matching the selector and if the requested instance exists
+    if (elements.length > instance) {
         // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth'});
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
     }
 }
 
@@ -12,15 +12,15 @@ const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
-link1.addEventListener('click', () =>{
+link1.addEventListener('click', () => {
     scrollToElement('.header');
-})
+});
 
-link2.addEventListener('click', () =>{
+link2.addEventListener('click', () => {
     // Scroll to the second element with "header" class
     scrollToElement('.header', 1);
-})
+});
 
-link3.addEventListener('click', () =>{
-    scrollToElement('.column');
-})
+link3.addEventListener('click', () => {
+    scrollToElement('.container:nth-of-type(4)');
+});
